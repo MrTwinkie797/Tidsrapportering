f48136b87a5048ebce9bb630c11a88631028091c
Adrian Tallgård <adrian.tallgard@gmail.com>
2016-11-30 20:05:59 +0100

    First commit

---
+++ TidsRapportering/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TidsRapportering.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TidsRapportering/Controllers/ReportsAPIController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TidsRapportering.Models;
+
+namespace TidsRapportering.Controllers
+{
+    public class ReportsAPIController : ApiController
+    {
+        private ReportTimeEntities db = new ReportTimeEntities();
+
+        // GET: api/ReportsAPI
+        public IQueryable<Report> GetReport()
+        {
+            return db.Report;
+        }
+
+        // GET: api/ReportsAPI/5
+        [ResponseType(typeof(Report))]
+        public async Task<IHttpActionResult> GetReport(int id)
+        {
+            Report report = await db.Report.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(report);
+        }
+
+        // PUT: api/ReportsAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutReport(int id, Report report)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != report.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(report).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ReportsAPI
+        [ResponseType(typeof(Report))]
+        public async Task<IHttpActionResult> PostReport(Report report)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Report.Add(report);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReportExists(report.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = report.Id }, report);
+        }
+
+        // DELETE: api/ReportsAPI/5
+        [ResponseType(typeof(Report))]
+        public async Task<IHttpActionResult> DeleteReport(int id)
+        {
+            Report report = await db.Report.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            db.Report.Remove(report);
+            await db.SaveChangesAsync();
+
+            return Ok(report);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReportExists(int id)
+        {
+            return db.Report.Count(e => e.Id == id) > 0;
+        }
+    }
+}
